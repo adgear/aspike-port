15bba3e2699607622b36734818e78931b9ec6862
Dmitriy Kargapolov <d.kargapolov@samsung.com>
2025-04-10 23:01:17 -0400

    Use cf_alloc/free for code consistency

--- c_src/port/aerocalls.c
+++ c_src/port/aerocalls.c
@@ -29,6 +29,7 @@
 #include <aerospike/as_exp.h>
 #include <aerospike/as_batch.h>
 #include <aerospike/aerospike_batch.h>
+#include <citrusleaf/alloc.h>
 
 // ----------------------------------------------------------------------------
 
@@ -976,7 +977,7 @@ static void dump_bin(ei_x_buff *p_res_buf, const as_bin* p_bin) {
         default:
             val_as_str = as_val_tostring(as_bin_get_value(p_bin));
             ei_x_encode_string(p_res_buf, val_as_str);
-            free(val_as_str);
+            cf_free(val_as_str);
     }
 }
 
@@ -1022,7 +1023,7 @@ static void dump_binary_bin(ei_x_buff *p_res_buf, const as_bin* p_bin) {
         default:
             val_as_str = as_val_tostring(as_bin_get_value(p_bin));
             ei_x_encode_string(p_res_buf, val_as_str);
-            free(val_as_str);
+            cf_free(val_as_str);
     }
 }
 
@@ -1156,7 +1157,7 @@ static void format_value_out(ei_x_buff *p_res_buf, as_val_t type, as_bin_value *
         default:
             val_as_str = as_val_tostring(val);
             ei_x_encode_string(p_res_buf, val_as_str);
-            free(val_as_str);
+            cf_free(val_as_str);
     }
 
 }
@@ -1169,7 +1170,7 @@ static int dump_cdt_records(ei_x_buff *p_res_buf, const as_record *p_rec) {
     } else if (p_rec->key.valuep) {
 	    char* key_val_as_str = as_val_tostring(p_rec->key.valuep);
         OKP(key_val_as_str)
-	    free(key_val_as_str);
+	    cf_free(key_val_as_str);
         return res;
     } else {
 
@@ -1211,7 +1212,7 @@ static int binary_dump_records(ei_x_buff *p_res_buf, const as_record *p_rec) {
     if (p_rec->key.valuep) {
 	char* key_val_as_str = as_val_tostring(p_rec->key.valuep);
         OKP(key_val_as_str)
-	free(key_val_as_str);
+	cf_free(key_val_as_str);
         return res;
     }
 
@@ -1239,7 +1240,7 @@ static int dump_records(ei_x_buff *p_res_buf, const as_record *p_rec) {
 	if (p_rec->key.valuep) {
 		char* key_val_as_str = as_val_tostring(p_rec->key.valuep);
         OKP(key_val_as_str)
-		free(key_val_as_str);
+		cf_free(key_val_as_str);
         return res;
 	}
 
@@ -1404,7 +1405,7 @@ int call_key_select(const char *buf, int *index, int arity, int fd_out) {
             ERROR("invalid bin")
             goto end;
         }
-        bins[i] = strdup(bin);
+        bins[i] = (const char *)cf_strdup(bin);
     }
     bins[i] = NULL;
 
@@ -1424,7 +1425,7 @@ int call_key_select(const char *buf, int *index, int arity, int fd_out) {
         as_record_destroy(p_rec);
     }
     for (int j = 0; j < i; j++) {
-        free((void *)bins[j]);
+        cf_free((void *)bins[j]);
     }
 
     POST
@@ -1576,7 +1577,7 @@ int call_node_info(const char *buf, int *index, int arity, int fd_out) {
     ei_x_encode_list_header(&res_buf, 1);
     ei_x_encode_string(&res_buf, &info[0]);
     ei_x_encode_empty_list(&res_buf);
-    free(info);
+    cf_free(info);
 
     as_node_release(node);
     POST
@@ -1644,7 +1645,7 @@ int call_host_info(const char *buf, int *index, int arity, int fd_out) {
     ei_x_encode_list_header(&res_buf, 1);
     ei_x_encode_string(&res_buf, &info[0]);
     ei_x_encode_empty_list(&res_buf);
-    free(&info[0]);
+    cf_free(&info[0]);
 
 
     POST
@@ -1676,7 +1677,7 @@ int call_help(const char *buf, int *index, int arity, int fd_out) {
     }
 
     OK(info)
-    free(info);
+    cf_free(info);
 
     POST
 }
